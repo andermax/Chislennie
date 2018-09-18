d8956a4bf5d8ae13a2c2c7703ae8c8435811b03c
andermax <maks@cateye.lv>
2018-09-18 21:20:06 +0300

    18-09-2018 manualy

--- src/lv/tsi/metodi/Calculator.java
+++ src/lv/tsi/metodi/Calculator.java
@@ -5,6 +5,8 @@ import lv.tsi.metodi.printers.IPrinter;
 import lv.tsi.metodi.readers.IReader;
 import lv.tsi.metodi.readers.PredefinedReader;
 
+import java.lang.reflect.Array;
+
 import static java.lang.Math.*;
 
 public class Calculator{
@@ -36,13 +38,24 @@ public class Calculator{
     }
     public double[][] forwardGauss(double[][] values){
         //razdelenie na matricu A i vector B
-        double[][] newValues = null;
+        double[][] newValues = values;
         int n = values.length;
 
-        newValues[1][0] = values[1][0]*(-1 * values[1][0] / values[0][0]);
-        newValues[1][1] = values[1][0]*(-1 * values[1][0] / values[0][0]);
+        newValues[1][0] = values[0][0]*(-1 * values[1][0] / values[0][0]) + values[1][0];
+        newValues[1][1] = values[0][1]*(-1 * values[1][0] / values[0][0]) + values[1][1];
+        newValues[1][2] = values[0][2]*(-1 * values[1][0] / values[0][0]) + values[1][2];
+        newValues[1][1] = values[0][3]*(-1 * values[1][0] / values[0][0]) + values[1][3];
 
+        values = newValues;
 
+        newValues[2][0] = values[0][0]*(-1 * values[2][0] / values[0][0]) + values[2][0];
+        newValues[2][1] = values[0][1]*(-1 * values[2][0] / values[0][0]) + values[2][1];
+        newValues[2][2] = values[0][2]*(-1 * values[2][0] / values[0][0]) + values[2][2];
+        newValues[2][3] = values[0][3]*(-1 * values[2][0] / values[0][0]) + values[2][3];
+
+        values = newValues;
+
+        values[1][0]
 
 
 
